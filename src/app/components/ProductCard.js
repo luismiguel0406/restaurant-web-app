91cf521b615cb9d7790139889f79198ea54a6af1
Luis del orbe <luismigueldelorbe@hotmail.com>
2024-12-04 12:29:01 -0400

    working on layout

---
+++ src/app/components/ProductCard.js
@@ -0,0 +1,34 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+} from "@mui/material";
+
+const ProductCard = ({product}) => {
+
+  
+  return (
+    <Card sx={{maxWidth : 300}}>
+      <CardMedia 
+      title ='product'
+      image={product.imageUrl}
+      sx={{height:140}}
+      />
+      <CardContent>
+        <Typography variant="h5" component="div">
+         {product.strIngredient}
+        </Typography>
+        <Typography variant="h5">$ {product.price}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">+</Button>
+        <Button size="small">-</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProductCard;
