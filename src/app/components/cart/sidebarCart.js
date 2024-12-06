6c2680b9bc0d74a6cbf8bd1a7c583f9fcc33e10b
Luis del orbe <luismigueldelorbe@hotmail.com>
2024-12-05 22:08:24 -0400

    fix removing items from cart

--- src/app/components/cart/sidebarCart.js
+++ src/app/components/cart/sidebarCart.js
@@ -1,21 +1,24 @@
-import { Card, Grid2, CardMedia, CardContent, Typography, IconButton, CardActions } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Grid2 } from "@mui/material";
 import EmptyCart from "./emptyCart";
 import MiniCardProduct from "./miniCardProduct";
+import { useState } from "react";
 
 const SidebarCart = () => {
+
   let items = JSON.parse(localStorage.getItem("cart"));
+  const [itemsCart, setItemsCart] = useState(items)
 
 const handleRemoveFromCart = (idIngredient)=>{
 
   const filteredItems = items.filter((item)=>item.idIngredient !== idIngredient);
   localStorage.setItem("cart",JSON.stringify(filteredItems));
+  setItemsCart(filteredItems);
 };
-  if (!items) return <EmptyCart />;
+  if (!itemsCart) return <EmptyCart />;
 
   return (
     <Grid2 container spacing={2} sx={{ overflow: "hidden", p: 2, width: 350 }}>
-      {items?.map((item) => (
+      {itemsCart?.map((item) => (
         <Grid2 size={{xs:12}} sx={{height:85}} key={item.idIngredient}>
           <MiniCardProduct item={item} removeFromCart={handleRemoveFromCart}/>
         </Grid2>
