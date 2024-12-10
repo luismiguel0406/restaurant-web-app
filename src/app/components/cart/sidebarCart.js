91a8930f5104f5c8c4c9de58ca1aee4678e57c23
Luis del orbe <luismigueldelorbe@hotmail.com>
2024-12-10 17:52:27 -0400

    fix sideBartcart, products took all space between them

--- src/app/components/cart/sidebarCart.js
+++ src/app/components/cart/sidebarCart.js
@@ -42,10 +42,11 @@ const SidebarCart = ({ closeDrawer, clientId, socketId }) => {
 
   return (
     <Box width={350} height={"100vh"}>
+      <Box sx={{height: "75%", width:"100%"}}>
       <Grid2
         container
         spacing={2}
-        sx={{ overflow: "auto", p: 2, width: "100%", height: "75%" }}
+        sx={{ overflow: "auto", p: 2, width: "100%", maxHeight:"100%" }}
       >
         {itemsCart?.map((item) => (
           <Grid2 size={{ xs: 12 }} sx={{ height: 85 }} key={item.idIngredient}>
@@ -56,7 +57,7 @@ const SidebarCart = ({ closeDrawer, clientId, socketId }) => {
           </Grid2>
         ))}
       </Grid2>
-      
+      </Box>
       <Box width={"100%"} sx={{ p: 2, height: "25%" }}>
         <PaymentSummary items={items} />
         <Button
